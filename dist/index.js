71fbf61700ed341ee8aecacce76f20b1c6570490
RyanSu <563607356@qq.com>
2020-11-22 21:22:15 +0800

    222

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59869" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58067" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -238,7 +238,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59869" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58067" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
